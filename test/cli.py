81d46897420de27df7c1b6a68cd6f644cf021e85
golden <120225883@qq.com>
2018-06-10 00:26:19 +0800

    something

--- test/cli.py
+++ test/cli.py
@@ -1,10 +1,45 @@
 # coding:utf-8
-from __future__ import absolute_import, unicode_literals
 import click
 
-__author__ = "golden"
-__date__ = '2018/6/9'
-import click
+
+class State(object):
+
+    def __init__(self):
+        self.verbosity = 0
+        self.debug = False
+
+
+pass_state = click.make_pass_decorator(State, ensure=True)
+
+
+def verbosity_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(State)
+        state.verbosity = value
+        return value
+
+    return click.option('-v', '--verbose', count=True,
+                        expose_value=False,
+                        help='Enables verbosity.',
+                        callback=callback)(f)
+
+
+def debug_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(State)
+        state.debug = value
+        return value
+
+    return click.option('--debug/--no-debug',
+                        expose_value=False,
+                        help='Enables or disables debug mode.',
+                        callback=callback)(f)
+
+
+def common_options(f):
+    f = verbosity_option(f)
+    f = debug_option(f)
+    return f
 
 
 @click.group()
@@ -13,14 +48,12 @@ def cli():
 
 
 @cli.command()
-@click.option('--port', default=8000)
-def runserver(port):
-    click.echo('Serving on http://127.0.0.1:%d/' % port)
+@common_options
+@pass_state
+def cmd1(state):
+    click.echo('Verbosity: %s' % state.verbosity)
+    click.echo('Debug: %s' % state.debug)
 
 
 if __name__ == '__main__':
-    cli(default_map={
-        'runserver': {
-            'port': 5000
-        }
-    })
+    cli()
